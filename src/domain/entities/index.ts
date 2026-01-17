642feb203284555689357fbb2648ba713a5cafeb
AlessandroFMello <ale_95_f.mello@hotmail.com>
2026-01-17 16:33:51 -0300

    feat(domain): implements Work entity
    Signed-off-by: AlessandroFMello <ale_95_f.mello@hotmail.com>

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -2,3 +2,4 @@ export { Category } from './Category.js';
 export { Supplier } from './Supplier.js';
 export { Supply } from './Supply.js';
 export { Quote } from './Quote.js';
+export { Work } from './Work.js';
