5b7b6ae554ebb6bf1c7381d61d87035ac1701a62
AlessandroFMello <ale_95_f.mello@hotmail.com>
2026-01-17 11:46:02 -0300

    chore(infrastructure): configures Prisma as ORM
    Signed-off-by: AlessandroFMello <ale_95_f.mello@hotmail.com>

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "prisma/config";
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  migrate: {
-    datasourceUrl: process.env.DATABASE_URL,
+  schema: './prisma/schema.prisma',
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+  migrations: {
+    path: 'prisma/migrations',
   },
 });
