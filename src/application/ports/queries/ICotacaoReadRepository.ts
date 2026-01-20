824176735987e7aa128ceaa0f2d4770108365704
AlessandroFMello <ale_95_f.mello@hotmail.com>
2026-01-19 21:49:41 -0300

    feat(application): add repository methods for categories and inspections

--- src/application/ports/queries/ICotacaoReadRepository.ts
+++ src/application/ports/queries/ICotacaoReadRepository.ts
@@ -1,6 +1,8 @@
 import type { MenorPrecoInsumoDTO } from '../../dtos/MenorPrecoInsumoDTO.js';
+import type { CheapestQuoteDTO } from '../../dtos/CheapestQuoteDTO.js';
 
 export interface ICotacaoReadRepository {
   findCheapestBySupplyId(supplyId: string): Promise<MenorPrecoInsumoDTO>;
+  findCheapestByWorkId(workId: string): Promise<CheapestQuoteDTO>;
 }
 
